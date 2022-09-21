55c89135b0c7411679f13ae00c7b1c0e8aa2af40
David Nicholas <Dnicholas@bitovi.com>
2022-09-21 10:22:38 -0700

    update application

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
-// @ts-nocheck
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Start Exercise Here
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
