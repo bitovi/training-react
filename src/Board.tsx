14ea6240e3ff1b3e351e6a8e92f849b7ec2e7356
David Nicholas <Dnicholas@bitovi.com>
2022-10-06 16:20:50 -0700

    add solution

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,14 +1,37 @@
+import type { TicTacToeBoard } from "@utilities/ticTacToe";
+
+import { useState } from "react";
+
 import Square from "./Square";
 
 import styles from "./Board.module.css";
 
-const board = Array(9).fill(null);
+const initialBoard = Array(9).fill(null);
 
 const Board = () => {
+  const [board, setBoard] = useState<TicTacToeBoard>(initialBoard);
+  const [isX, setIsX] = useState(true);
+
+  const handleSquareSelect = (selectedSquare: number) => {
+    if (board[selectedSquare]) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[selectedSquare] = isX ? "X" : "O";
+
+    setBoard(newBoard);
+    setIsX(!isX);
+  };
+
   return (
     <div className={styles.board}>
-      {board.map((squareValue) => (
-        <Square value={squareValue} />
+      {board.map((squareValue, index) => (
+        <Square
+          key={index}
+          value={squareValue}
+          onSquareSelect={() => handleSquareSelect(index)}
+        />
       ))}
     </div>
   );
--- src/Square.tsx
+++ src/Square.tsx
@@ -3,11 +3,16 @@ import type { BoardSquareValue } from "@utilities/ticTacToe";
 import styles from "./Square.module.css";
 
 interface SquareProps {
+  onSquareSelect: () => void;
   value: BoardSquareValue;
 }
 
-const Square = ({ value }: SquareProps) => {
-  return <div className={styles.square}>{value}</div>;
+const Square = ({ value, onSquareSelect }: SquareProps) => {
+  return (
+    <div className={styles.square} onClick={() => onSquareSelect()}>
+      {value}
+    </div>
+  );
 };
 
 export default Square;
