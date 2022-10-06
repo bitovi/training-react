d3e8779ecc8613263442e162ac0553c5ab7c6c70
David Nicholas <Dnicholas@bitovi.com>
2022-10-06 15:16:01 -0700

    add solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,24 @@
 // @ts-nocheck
 import ReactDOM from "react-dom/client";
 
-// Start Exercise Here
+// Hello World from React
+const element = (
+  <>
+    <h2>Hello World</h2> <p>from React</p>
+  </>
+);
+
+// const element = (
+//   <>
+//     {[1, 2, 3, 4, 5].map((number) => (
+//       <button key={number} onClick={() => alert(number)}>
+//         {number}
+//       </button>
+//     ))}
+//   </>
+// );
+
+const rootNode = document.getElementById("root");
+const renderRoot = ReactDOM.createRoot(rootNode);
+
+renderRoot.render(element);
