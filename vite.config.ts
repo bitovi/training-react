838e0fe5cfbcd24677f60c2e312f90678c567c4b
David Nicholas <Dnicholas@bitovi.com>
2022-10-11 09:42:34 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "training-react",
+  base: "/training-react",
   plugins: [react()],
   resolve: {
     alias: [
