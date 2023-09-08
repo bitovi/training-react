580c4eeb3730d3011345f14e1c991c342321aad2
Chasen Le Hara <chasen@bitovi.com>
2023-09-07 18:36:10 -0700

    Add the click handler solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,15 @@
 // @ts-nocheck
 import ReactDOM from "react-dom/client";
 
-// Hello World from React
 const element = (
   <>
-    <h2>Hello World</h2> <p>from React</p>
+    {[1, 2, 3, 4, 5].map((number) => (
+      <button key={number} onClick={() => alert(number)}>
+        {number}
+      </button>
+    ))}
   </>
 );
-
-// const element = (
-//   <>
-//     {[1, 2, 3, 4, 5].map((number) => (
-//       <button key={number} onClick={() => alert(number)}>
-//         {number}
-//       </button>
-//     ))}
-//   </>
-// );
 
 const rootNode = document.getElementById("root");
 const renderRoot = ReactDOM.createRoot(rootNode);
